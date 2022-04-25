0a9eef34b9827b79a8bdee1404080ad06aa0e7f2
nimamac <1132957271@qq.com>
2022-04-25 21:35:01 +0800

    主页基本完成

--- pages/sport/sport.vue
+++ pages/sport/sport.vue
@@ -1,130 +1,157 @@
 <template>
-	<view>
-		<view class="">
+    <view>
+        <view class="">
 
-			<view class="">
-				<view class="color-1129 text-bold margin-sm type-text" v-for="(item,index) in typeList" :key="index">
-					<view class="margin-bottom-sm ">
-						{{item}}
-					</view>
-					<view class="grid" v-if="item === '有氧运动'">
-						
+            <view class="">
+                <view class="color-1129 text-bold margin-sm type-text" v-for="(item,index) in typeList" :key="index">
+                    <view class="margin-bottom-sm ">
+                        {{item}}
+                    </view>
+                    <view class="grid" v-if="item === '有氧运动'">
 
-						<view
-							v-for="(item,index) in youyangList" :key="index" @click="handleSportClick(item)">
-							<!-- {{item.name}} -->
-								<image class="sport-image" v-if="item.picture" :src="item.picture" mode=""></image>
-						</view>
-					</view>
-					
-					<view class="grid" v-if="item === '柔韧训练'">
-					
-						<view :class="['text-xl','text-center','sportText']" hover-class="sportTextClick"
-							v-for="(item,index) in rourenList" :key="index" @click="handleSportClick(item)">
-							{{item.name}}
-						</view>
-					</view>
-					
-					<view class="grid" v-if="item === '抗阻训练'">
-					
-						<view :class="['text-xl','text-center','sportText']" hover-class="sportTextClick"
-							v-for="(item,index) in kangzuList" :key="index" @click="handleSportClick(item)">
-							{{item.name}}
-						</view>
-					</view>
-					<u-divider></u-divider>
 
-				</view>
-			</view>
+                        <view v-for="(item,index) in youyangList" :key="index" @click="handleSportClick(item)">
+                            <!-- {{item.name}} -->
+                            <view class="abs-contianer" v-if="item.picture">
+                                <image class="sport-image" :src="item.picture" mode=""></image>
+                                <view class="sport-span">{{item.name}}</view>
+                            </view>
 
-			<!-- <button type="default" @click="getName">123</button> -->
-		</view>
-	</view>
+                            <!-- 没有图片的显示 -->
+
+                            <view v-else-if="item.picture === null" :class="['text-xl','text-center','sportText']"
+                                hover-class="sportTextClick">
+                                <text class="font-max">{{item.name}}</text>
+                            </view>
+
+                            <view  :class="['text-xl','text-center','sportText']">
+                                <text class="font-max">已完成
+                                </text>
+                            </view>
+
+
+                        </view>
+                    </view>
+
+                    <view class="grid" v-if="item === '柔韧训练'">
+
+                        <view :class="['text-xl','text-center','sportText']" hover-class="sportTextClick"
+                            v-for="(item,index) in rourenList" :key="index" @click="handleSportClick(item)">
+                            <view class="type-text">
+                                {{item.name}}
+                            </view>
+                        </view>
+                    </view>
+
+                    <view class="grid" v-if="item === '抗阻训练'">
+
+                        <view :class="['text-xl','text-center','sportText']" hover-class="sportTextClick"
+                            v-for="(item,index) in kangzuList" :key="index" @click="handleSportClick(item)">
+                            {{item.name}}
+                        </view>
+                    </view>
+                    <u-divider></u-divider>
+
+                </view>
+            </view>
+
+            <!-- <button type="default" @click="getName">123</button> -->
+        </view>
+    </view>
 </template>
 
 <script>
-	import {
-		getSportType,
-		gerNameByType
-	} from '../../api/sport.js'
-	export default {
-		data() {
-			return {
-				typeList: ['有氧运动', '柔韧训练', '抗阻训练'],
-				youyangList: [],
-				rourenList: [],
-				kangzuList: []
-			};
-		},
-		onLoad() {
-			this.getTypeList()
-		},
-		methods: {
-			// 获取运动类型
-			async getTypeList() {
-				const res = await getSportType()
-				console.log(res)
-				if (res) {
-					this.typeList = res['type']
-				}
-				for (let item in this.typeList) {
-					this.getName(this.typeList[item])
-				}
-			},
-			async getName(type) {
-				const res = await gerNameByType({
-					params: {
-						type
-					}
-				})
-				if (res) {
-					console.log(res)
-					if(type === '有氧运动'){
-						this.youyangList = res.name
-					}else if(type === '柔韧训练'){
-						this.rourenList = res.name
-					}else if(type === '抗阻训练'){
-						this.kangzuList = res.name
-					}
-				}
-			},
-			// 处理单击运动
-			handleSportClick(item) {
-				console.log(item)
-				uni.navigateTo({
-					url: '/pages/sport/sprotDetail?item=' + item.name,
-				})
-			}
-		}
-	}
+    import {
+        getSportType,
+        gerNameByType
+    } from '../../api/sport.js'
+    export default {
+        data() {
+            return {
+                typeList: [],
+                youyangList: [],
+                rourenList: [],
+                kangzuList: []
+            };
+        },
+        onLoad() {
+            this.getTypeList()
+        },
+        methods: {
+            // 获取运动类型
+            async getTypeList() {
+                const res = await getSportType()
+                console.log(res)
+                if (res) {
+                    this.typeList = res['type']
+                }
+                for (let item in this.typeList) {
+                    this.getName(this.typeList[item])
+                }
+            },
+            async getName(type) {
+                const res = await gerNameByType({
+                    params: {
+                        type
+                    }
+                })
+                if (res) {
+                    console.log(res)
+                    if (type === '有氧运动') {
+                        this.youyangList = res.name
+                    } else if (type === '柔韧训练') {
+                        this.rourenList = res.name
+                    } else if (type === '抗阻训练') {
+                        this.kangzuList = res.name
+                    }
+                }
+            },
+            // 处理单击运动
+            handleSportClick(item) {
+                console.log(item)
+                uni.navigateTo({
+                    url: '/pages/sport/sprotDetail?item=' + item.name,
+                })
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-	.sportText {
-		width: 80vw;
-		height: 20vh;
-		line-height: 20vh;
-		// margin: 0 auto;
-		color: #F0F0F0;
-		background-color: #007aff;
-		border-radius: 20upx;
-	}
+    .sportText {
+        width: 80vw;
+        height: 30vh;
+        line-height: 30vh;
+        color: #F0F0F0;
+        background-color: #007aff;
+        border-radius: 20upx;
+    }
 
-	.sportTextClick {
-		color: #000000;
-		background-color: #f0f0f0;
-	}
+    .sportTextClick {
+        color: #000000;
+        background-color: #f0f0f0;
+    }
 
-	.type-text {
-		font-size: 56upx;
-	}
-	
-	.sport-image{
-		width: 80vw;
-		height: 30vh;
-		display: inline-block;
-		    border: 1px solid #ddd;
-		    border-radius: 4px;
-		    padding: 5px;
-	}
+    .type-text {
+        font-size: 56upx;
+    }
+
+    .abs-contianer {
+        position: relative;
+    }
+
+    .sport-image {
+        width: 80vw;
+        height: 30vh;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        padding: 5px;
+    }
+
+    .sport-span {
+        position: absolute;
+        color: white;
+        top: 35%;
+        left: 35%;
+    }
 </style>
--- pages/state/state.vue
+++ pages/state/state.vue
@@ -1,26 +1,73 @@
 <template>
-	<view>
-		<view class="flex justify-between ">
-			<button type="default" @click="goHealthAssess">健康测评</button>
-		</view>
-	</view>
+    <view >
+        
+
+
+       <circle-component :progress_txt="stepPercent">
+       </circle-component>
+      
+    
+       
+       <view class="grid margin-top-xl">
+           <view class="border text-contianer" @click="goHealthAssess">
+               <view class="flex flex-direction align-center">
+                   <u-icon name="heart-fill" color="white" size="38"></u-icon>
+                   <text class="font-max margin-top-sm">健康测评</text>
+               </view>
+           </view>
+           
+           <view class="border text-contianer">
+               <view class="flex flex-direction align-center">
+                   <u-icon name="man-add" color="white" size="38"></u-icon>
+                   <text class="font-max margin-top-sm">健康汇报</text>
+               </view>
+           </view>
+       </view>
+      
+
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		},
-		methods:{
-			goHealthAssess(){
-				this.$u.route('/pages/state/healthAssess')
-			}
-		}
-	}
+    import circleComponent from '../../components/circleComponent.vue'
+    export default {
+        components:{
+            circleComponent
+        },
+        data() {
+            return {
+                stepPercent:90
+            };
+        },
+        mounted() {
+            this.drawProgressbg();
+            this.drawCircle(this.progress_txt); //参数为1-100
+            this.drawLine();
+        },
+        methods: {
+            goHealthAssess() {
+                this.$u.route('/pages/state/healthAssess')
+            },
+           
+        }
+    }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+    .health-contianer{
+        margin-top: 50upx;
+        font-size: 60upx;
+    }
+    
+    .text-contianer{
+        width: 40vw;
+        height: 20vh;
+        margin: 30upx;
+        border-radius: 30upx;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #F0F0F0;
+        background-color: #007aff;
+    }
 </style>
